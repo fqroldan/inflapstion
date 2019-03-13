0f2ec501fdc70626352f80c3f1d332d3c88be6be
qurufinwe <fr673@nyu.edu>
2019-03-13 11:10:51 -0400

    added establish_remote()

--- ct.jl
+++ ct.jl
@@ -522,13 +522,22 @@ function plot_simul(ct::CrazyType; T::Int64=50, jp0::Int64=2)
 
     return p
 end
-write(pwd()*"/../output.txt", "")
+# write(pwd()*"/../output.txt", "")
 
+function establish_remote()
+	machine_name = ""
+	try
+		machine_name = read("/name.txt", String)
+	catch
+	end
+	return !(machine_name=="qlaptop")
+end
+machine_remote = establish_remote()
 
 _loopω = true
 
 if _loopω
-	L_mat, ωmin, p1 = choose_ω(; remote = true)
+	L_mat, ωmin, p1 = choose_ω(; remote = machine_remote)
 	p1
 
 	ct = CrazyType(; ω = ωmin)
