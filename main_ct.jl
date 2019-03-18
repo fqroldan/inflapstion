93e73b52ef18dde3c6c7ed8c9780627791844e44
qurufinwe <fr673@nyu.edu>
2019-03-18 09:43:19 -0400

    Housekeeping

---
+++ main_ct.jl
@@ -0,0 +1,22 @@
+using Distributed
+
+@everywhere include("ct.jl")
+@everywhere include("reporting_routines.jl")
+@everywhere include("type_def.jl")
+@everywhere include("plotting_routines.jl")
+
+write(pwd()*"/../output.txt", "")
+
+function establish_remote()
+	machine_name = ""
+	try
+		machine_name = read("/name.txt", String)
+	catch
+	end
+	return !(machine_name=="qlaptop")
+end
+machine_remote = establish_remote()
+
+
+L_mat, ωmin, p1 = choose_ω(; remote = machine_remote)
+p1
---
+++ simul.jl
@@ -0,0 +1,44 @@
+function iter_simul(ct::CrazyType, itp_gπ, pv, av; noshocks::Bool=false)
+	ϵ = rand(dist_ϵ(ct))
+	if noshocks
+		ϵ = 0.0
+	end
+
+	exp_π = itp_gπ(pv, av)
+	obs_π = exp_π+ϵ
+	
+	pprime = Bayes(ct, obs_π, exp_π, pv, av)
+
+	aprime = ϕ(ct, av)
+	exp_π′ = pprime * aprime + (1.0-pprime) * itp_gπ(pprime, aprime)
+
+	y = NKPC(ct, obs_π, exp_π′)
+
+	return pprime, aprime, obs_π, y, exp_π, ϵ
+end
+
+function simul(ct::CrazyType; T::Int64=50, jp0::Int64=2, noshocks::Bool=false)
+	p0 = ct.pgrid[jp0]
+
+	_, ind_a0 = findmin(ct.L[jp0, :])
+	a0 = ct.agrid[ind_a0]
+
+	p, a = p0, a0
+
+	knots = (ct.pgrid, ct.agrid)
+	itp_gπ = interpolate(knots, ct.gπ, Gridded(Linear()))
+
+	p_vec, a_vec, π_vec, y_vec, g_vec, ϵ_vec = zeros(T), zeros(T), zeros(T), zeros(T), zeros(T), zeros(T)
+	for tt = 1:T
+		p_vec[tt], a_vec[tt] = p, a
+		pp, ap, πt, yt, gt, ϵt = iter_simul(ct, itp_gπ, p, a; noshocks=noshocks)
+		π_vec[tt], y_vec[tt], g_vec[tt], ϵ_vec[tt] = πt, yt, gt, ϵt
+
+		p, a = pp, ap
+	end
+
+	σϵ = std(ϵ_vec)
+	print("\nStd of shocks = $(@sprintf("%.3g", σϵ))")
+
+	return p_vec, a_vec, π_vec, y_vec, g_vec
+end
