00d59f557d0f6f372acd4993304f55bbaf395e8d
Francisco Roldan <fr673@nyu.edu>
2019-11-13 14:36:28 -0500

    Fixed grids

--- type_def.jl
+++ type_def.jl
@@ -58,7 +58,7 @@ function CrazyType(T::DataType;
 		κ = 1/κ
 	end
 
-	A = κ / (1.0 - β + κ^2*γ) * ystar
+	A = Nash(T, β, γ, κ, ystar)
 
 	pgrid = cdf.(Beta(5,2), range(0,1,length=Np))
 	agrid = cdf.(Beta(2,2), range(0,1,length=Na))
@@ -77,8 +77,11 @@ end
 
 ϕ(ct::CrazyType, a::Float64) = exp(-ct.ω) * (a-ct.χ) + ct.χ
 
-Nash(ct::CrazyType{Forward}) = ct.κ / (1.0 - ct.β + ct.κ^2*ct.γ) * ct.ystar
-Nash(ct::CrazyType{Backward}) = ct.κ / ct.γ * ct.ystar
+Nash(T::Backward, β, γ, κ, ystar) = κ / γ * ystar
+Nash(T::Forward, β, γ, κ, ystar) = κ / (1.0 - β + κ^2*γ) * ystar
+
+Nash(ct::CrazyType{Forward}) = Nash(Forward, ct.κ, ct.γ, ct.ystar, ct.β)
+Nash(ct::CrazyType{Backward}) = Nash(Backward, ct.κ, ct.γ, ct.ystar, ct.β)
 
 dist_ϵ(ct) = Normal(0, ct.σ)
 pdf_ϵ(ct, ϵv) = pdf.(dist_ϵ(ct), ϵv)
