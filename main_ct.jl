51a4823d57cf9d4a8fe6109e2837bf7cbf8112e4
Francisco Roldan <fr673@nyu.edu>
2019-11-14 21:44:59 -0500

    Fewer \omega points

--- main_ct.jl
+++ main_ct.jl
@@ -45,7 +45,7 @@ ct = create_or_load(Backward)
 initial_report(ct)
 
 # Epfi!(ct, tol=1e-4, tempplots=true, upd_η = 0.1)
-Nω = 25
+Nω = 20
 Nχ = 15 
 print_save("\nNω, Nχ = $Nω, $Nχ")
 L_mat = zeros(Nω, Nχ, ct.Np, ct.Na)
