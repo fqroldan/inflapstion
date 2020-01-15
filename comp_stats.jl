5c4be519a297793fd989b8ec9d37da12d54d0290
Francisco Roldan <fr673@nyu.edu>
2020-01-15 14:05:38 -0500

    Prepared code for comp_statics

---
+++ comp_stats.jl
@@ -0,0 +1,75 @@
+using Distributed, JLD
+
+function qload(s)
+	if length(s) == 2
+		s1 = parse(Int, s[1])
+		s2 = parse(Int, s[2])
+	else
+		throw(error("ARGS is too long (length = $(length(s))"))
+	end
+
+	return s1, s2
+end
+function prepare_results(run_number)
+	if run_number == 1
+		write("../../compstats.txt", "a,omega,chi,Lmin\n")
+	end
+	nothing
+end
+
+run_number, Nruns = qload(ARGS)
+prepare_resuts(run_number)
+
+include("ct.jl")
+σvec = range(0.005/4, 0.02/4, length=Nruns)
+σs = σvec[run_number]
+
+write(pwd()*"/../output.txt", "")
+write(pwd()*"/../temp.txt", "")
+
+function create_or_load(T::DataType)
+	ct = CrazyType(T, σ = σs, ω = 0.2, χ = 0.0);
+	try
+		print_save("Loading first file of previous run: ")
+		ctt = load("../../ct_1.jld", "ct")
+		if typeof(ctt) == typeof(ct) && ct.Np == ctt.Np && ct.Na == ctt.Na
+			ct.gπ=ctt.gπ
+		end
+		print_save("✓\n")
+	catch
+		print_save("failed.")
+		try
+			print_save("Loading optimum of previous run :")
+			ctt = load("../../ct_opt.jld", "ct");
+			if typeof(ctt) == typeof(ct) && ct.Np == ctt.Np && ct.Na == ctt.Na
+				ct.gπ=ctt.gπ
+			end
+			print_save("✓\n")
+		catch
+			print_save("failed. Using new type as guess.\n")
+		end
+	end
+	return ct
+end
+
+ct = create_or_load(Forward)
+
+initial_report(ct)
+
+function fill_in_results(a, ω, χ, Lmin)
+	s = read("../../compstats.txt", String)
+	write("../../compstats.txt", s*"$(a), $(ω), $(χ), $(Lmin)\n")
+	nothing
+end
+
+# Epfi!(ct, tol=1e-4, tempplots=true, upd_η = 0.1)
+Nω = 30
+Nχ = 15 
+print_save("\nNω, Nχ = $Nω, $Nχ")
+L_mat = zeros(Nω, Nχ, ct.Np, ct.Na)
+
+a, ω, χ = choose_ω!(L_mat, ct)
+Lmin = minimum(L_mat[:,:,3,:])
+fill_in_results(a, ω, χ, Lmin)
+
+nothing
---
+++ compstats_aps.s
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=7
+#SBATCH --mem=60GB
+#SBATCH --job-name=InflAPStion
+#SBATCH --time=5-10:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=froldan@nyu.edu
+#SBATCH --output=cddp_%A_%a.out
+#SBATCH --error=cddp_%A_%a.err
+
+x=$SLURM_ARRAY_TASK_ID
+
+module purge
+module load julia/1.3.0
+module load rclone/1.38
+echo $x
+echo $N
+
+mkdir -p run$x/
+cd run$x/
+
+git clone https://github.com/fqroldan/inflapstion.git
+cd inflapstion
+
+export JULIA_NUM_THREADS=7
+julia comp_stats.jl $x $N
