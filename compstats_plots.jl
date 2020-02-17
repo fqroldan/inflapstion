bee97fa4c80a59dfb3463cd1e91b02a5f726364c
Francisco Roldan <fr673@nyu.edu>
2020-02-17 10:04:50 -0500

    Minor improvements

--- compstats_plots.jl
+++ compstats_plots.jl
@@ -13,13 +13,21 @@ function makeplot_compstats(param::String; slides::Bool=true, temp::Bool=false)
 
 	palette = ColorSchemes.southwest
 
+	if param == "sigma"
+		xax = "σ"
+	elseif param == "beta"
+		xax = "β"
+	elseif param == "kappa"
+		xax = "κ"
+	end
+
 	yax = [""; "y2"; "y1"; "y1"]
 
 	ps = [scatter(x=df[!,1], y=df[!,jj], mode="lines", marker_color=get(palette, (jj-1)/(length(yax)-1)), name="𝔼["*varnames[jj]*"]", xaxis="x", yaxis=yax[jj]) for jj in 2:length(yax)]
 	# pω = scatter(x=df[!,1]*4, y=df[!,3], name=varnames[3], xaxis="x", yaxis="y2")
 
 	layout = Layout(
-		xaxis=attr(domain=[0,1],anchor="y", title="<i>$(varnames[1])"),
+		xaxis=attr(domain=[0,1],anchor="y", title="<i>"*xax),
 		yaxis1=attr(domain=[0,0.45], title="%"),
 		yaxis2=attr(domain=[0.55,1], anchor="x"),
 		legend=attr(orientation="h", x=0.05)
@@ -27,12 +35,14 @@ function makeplot_compstats(param::String; slides::Bool=true, temp::Bool=false)
 
 	p1 = plot(ps, layout)
 
-	p2 = plot(scatter(x=df[!,1], y=df[!,end], fill="tozeroy"), Layout(xaxis_title="<i>$(varnames[1])"))
+	p2 = plot(scatter(x=df[!,1], y=df[!,end], mode="lines", fill="tozeroy"), Layout(xaxis = attr(title="<i>"*xax, zeroline=false), yaxis_zeroline=false))
 	
 	if slides
 		relayout!(p1, plot_bgcolor="#fafafa", paper_bgcolor="#fafafa", font_family="Lato", font_size=16, width=900, height=600)
+		relayout!(p2, plot_bgcolor="#fafafa", paper_bgcolor="#fafafa", font_family="Lato", font_size=16, width=900, height=600)
 	else
 		relayout!(p1, font_family="Linux Libertine", font_size=16, width=700, height=350)
+		relayout!(p2, font_family="Linux Libertine", font_size=16, width=700, height=350)
 	end
 
 	return p1, p2
