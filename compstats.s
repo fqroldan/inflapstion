a970f889401705490043b0e787691c8687661c55
fqroldan <fr673@nyu.edu>
2021-02-23 11:41:28 -0500

    Local comparative statics

---
+++ compstats.s
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+N=$1
+param=$2
+
+sender(){
+	mkdir -p ../Output/CompStats/$param/run$1
+	cp comp_stats.jl ../Output/CompStats/$param/run$1/comp_stats.jl
+	cd ../Output/CompStats/$param/run$1
+	sleep 1
+	# export JULIA_NUM_THREADS=8
+	julia comp_stats.jl $1 $2 $3 &
+	echo "started run $1"
+	cd ../../../../Codes
+}
+
+rm -rf ../Output/CompStats/$param
+
+for ((i=1; i<=$N; i++))
+do
+	sender $i $N $param
+done
+
+wait
+
+echo "All done"
