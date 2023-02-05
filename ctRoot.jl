11dc6b3dcab90aa28c88c9f1f7fdfbdbc17b45cc
fqroldan <fr673@nyu.edu>
2023-02-05 14:58:08 -0500

    minor print

--- ctRoot.jl
+++ ctRoot.jl
@@ -278,7 +278,7 @@ function solve_all!(mt::MultiType; verbose = true)
 		mt.g_mat[jω, jχ, :, :] .= ct.gπ
 		
 		perc = 100 * iter / tot
-		verbose && print(" $perc% completed.\n")
+		verbose && print(" $(round(perc, sigdigits=2))% completed.\n")
 	end
 end
 
